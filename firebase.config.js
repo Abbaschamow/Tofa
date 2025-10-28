aade412b3b4cbee2352bdfde27b60e6715e28c07
Abbaschamow <148480235+Abbaschamow@users.noreply.github.com>
2025-10-28 22:31:48 +0100

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1fQdrmOJWDh4dEdQTFucj9BUeQmH_yew",
+  authDomain: "whatsapp-web-944f9.firebaseapp.com",
+  databaseURL: "https://whatsapp-web-944f9-default-rtdb.firebaseio.com",
+  projectId: "whatsapp-web-944f9",
+  storageBucket: "whatsapp-web-944f9.firebasestorage.app",
+  messagingSenderId: "975370289693",
+  appId: "1:975370289693:web:23390019b3b1c216a0fcf4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
